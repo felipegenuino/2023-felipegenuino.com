c3a2c065359e0f753832a1d75d6a33cc65d8c84d
Felipe Genuino <design@felipegenuino.com>
2023-05-14 17:26:24 -0300

    start: configure: mail, auth

---
+++ resources/views/emails/myTestMail.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>felipegenuino.com</title>
+</head>
+<body>
+    <h1>Olá</h1>
+    <p>Você consegue receber este email?</p>
+
+    <p>Thank you</p>
+</body>
+</html>
