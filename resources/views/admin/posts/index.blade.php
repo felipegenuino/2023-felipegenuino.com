62cec18db511b5ed12b846e7ce469a0ab72e0953
Felipe Genuino <design@felipegenuino.com>
2023-05-23 11:00:49 -0300

    info

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -111,7 +111,55 @@
 
 
                                         <a href="
-                                        {{-- {{route('admin.posts.edit', $post->id) }} --}}
+                                        {{-- ls
+{{route('admin.posts.edit', $post->id) }} --}}
+                                        "
+                                           title="Editar post: {{ $post->title }}">
+                                            <svg class="w-5" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                 viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                 class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                            </svg>
+                                        </a>
+
+                                    </div>
+                                </td>
+                            </tr>
+
+                        @empty
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <td class="px-6 py-4">
+                                    Nenhum post encontrado
+                                </td>
+                            </tr>
+                        @endforelse
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+
+        </div>
+
+
+    </div>
+
+
+
+    <div class="pt-4 pb-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div>
+                {{ $posts->links() }}
+            </div>
+        </div>
+    </div>
+
+
+
+</x-app-layout>
+{{route('admin.posts.edit', $post->id) }} --}}
                                         "
                                            title="Editar post: {{ $post->title }}">
                                             <svg class="w-5" xmlns="http://www.w3.org/2000/svg" fill="none"
