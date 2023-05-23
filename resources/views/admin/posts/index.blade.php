1ed752a4a853c48469bc3648fee226bbbf8c0e01
Felipe Genuino <design@felipegenuino.com>
2023-05-23 10:59:12 -0300

    update project

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -110,7 +110,9 @@
                                         </a>
 
 
-                                        <a href="{{ route('admin.posts.edit', $post->id) }}"
+                                        <a href="
+                                        {{-- {{route('admin.posts.edit', $post->id) }} --}}
+                                        "
                                            title="Editar post: {{ $post->title }}">
                                             <svg class="w-5" xmlns="http://www.w3.org/2000/svg" fill="none"
                                                  viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 
 
 Route::resources([
-//    'works' => WorkController::class,
+    // 'works' => WorkController::class,
     'posts' => PostController::class
 ]);
 
@@ -52,4 +52,4 @@ Route::middleware('auth')->group(function () {
 // });
 
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
